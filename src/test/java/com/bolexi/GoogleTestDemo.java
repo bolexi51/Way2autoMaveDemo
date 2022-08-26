3e53228713d9fe577357d502df865559dd2dcc6c
bolexi51 <abolade76@yahoo.com>
2022-08-26 19:32:06 +0200

    2 file inital commit

---
+++ src/test/java/com/bolexi/GoogleTestDemo.java
@@ -0,0 +1,15 @@
+package com.bolexi;
+
+import org.testng.annotations.Test;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+
+public class GoogleTestDemo {
+
+	
+	@Test
+	public void googleSearchTest() {	
+		
+	}
+}
+
---
+++ src/test/java/com/training/SampleDemos.java
@@ -0,0 +1,11 @@
+package com.training;
+
+import org.testng.annotations.Test;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+
+public class SampleDemos {
+  @Test
+  public void f() {
+  }
+}
